e6a766fec3e3d944a68097bf351145046766f443
sarahbaranski <sbaranski@gmail.com>
2021-02-25 20:50:06 -0600

    created a student controller with create action and a post route added jwt gem

---
+++ app/controllers/api/students_controller.rb
@@ -0,0 +1,17 @@
+class Api::StudentsController < ApplicationController
+  def create
+    student = Student.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      position_id: 1,
+    )
+    if student.save
+      render json: { message: "Student created successfully" }, status: :created
+    else
+      render json: { errors: student.errors.full_messages }, status: :bad_request
+    end
+  end
+end
