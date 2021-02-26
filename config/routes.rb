c06e200beab7cda6c7965ac3c11436f5ab0c37e0
sarahbaranski <sbaranski@gmail.com>
2021-02-25 21:16:00 -0600

    created sessions controller and route for login and JWT, created backend student authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     post "/students" => "students#create"
+    post "/sessions" => "sessions#create"
   end
 end
