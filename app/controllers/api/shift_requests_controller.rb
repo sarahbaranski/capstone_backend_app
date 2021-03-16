8266ef7394958dd40e1dbed732f6f6342dc9691e
sarahbaranski <sbaranski@gmail.com>
2021-03-15 20:21:52 -0500

    Added destroy action to shifts_request controller

--- app/controllers/api/shift_requests_controller.rb
+++ app/controllers/api/shift_requests_controller.rb
@@ -8,4 +8,10 @@ class Api::ShiftRequestsController < ApplicationController
     @shift_request.save
     render "show.json.jb"
   end
+
+  def destroy
+    shift_request = Shift_Request.find_by(id: params[:id])
+    shift_request.destroy
+    render json: {message: "Shift has been deleted."}
+  end
 end
