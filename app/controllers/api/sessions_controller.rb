548923fd1b6dd3ef48e0ea3719ce09916da31688
sarahbaranski <sbaranski@gmail.com>
2021-03-16 21:17:45 -0500

    Added supervisor create to sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -12,7 +12,20 @@ class Api::SessionsController < ApplicationController
       )
       render json: { jwt: jwt, email: student.email, student_id: student.id }, status: :created
     else
-      render json: {}, status: :unauthorized
+      supervisor = Supervisor.find_by(email: params[:email])
+      if supervisor && supervisor.authenticate(params[:password])
+        jwt = JWT.encode(
+          {
+            supervisor_id: supervisor.id, # the data to encode
+            exp: 300.days.from_now.to_i, # the expiration time
+          },
+          Rails.application.credentials.fetch(:secret_key_base), # the secret key
+          "HS256" # the encryption algorithm
+        )
+        render json: { jwt: jwt, email: supervisor.email, supervisor_id: supervisor.id }, status: :created
+      else
+        render json: {}, status: :unauthorized
+      end
     end
   end
 end
